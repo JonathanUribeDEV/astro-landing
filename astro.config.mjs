2b7cb0043444ebabb24d0b11ae168cd88ed4e5ce
Jonathan Pabon Uribe <jonathan.pabon@hasltda.com>
2024-01-01 22:19:05 -0500

    change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://JonathanUribeDEV.github.io',
+  site: 'https://jonathanuribedev.github.io',
   base: '/astro-landing',
 });
